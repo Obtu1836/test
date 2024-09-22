b531d78a48816816958c288376ad09f1b3f401a1
Obtu1836 <ss751157ss@163.com>
2024-09-22 14:31:59 +0800

    f

--- t2/2.py
+++ t2/2.py
@@ -2,8 +2,8 @@ import numpy as np
 from numpy.linalg import norm 
 
 x=np.random.rand(5)
-data=np.random.rand(10).reshape(2,5)
+data=np.random.rand(15).reshape(3,5)
 
-p=data[:,None]-data
+p=norm(data[:,None]-data,axis=2)
 
 print(p)
